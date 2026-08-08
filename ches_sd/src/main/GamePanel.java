862e1a5193a18109c4c56daa9bf1d6b223c1dc48
clrkxd <clarkiemelitar@gmail.com>
2026-08-08 22:41:32 +0800

    switched back to delta time for better performance

--- ches_sd/src/main/GamePanel.java
+++ ches_sd/src/main/GamePanel.java
@@ -33,55 +33,58 @@ public class GamePanel extends JPanel implements Runnable{
 		gt.start();
 	}
 	
-//	@Override
-//	public void run() {
-//		// TODO Auto-generated method stub
-//		// game loop
-//		double interval = 1000000000/FPS;
-//		double delta = 0;
-//		long lastTime = System.nanoTime();
-//		long currentTime;
-//		
-//		while (gt != null) {
-//			currentTime = System.nanoTime();
-//			
-//			delta += (currentTime - lastTime) / interval;
-//			lastTime = currentTime;
-//			
-//			if (delta >= 1) {
-//				update();
-//				repaint();
-//				delta--;
-//			}
-//		}
-//	}
-	
+	// delta time for better performance
 	@Override
 	public void run() {
-	    double drawInterval = 1000000000.0 / FPS;
-	    double nextDrawTime = System.nanoTime() + drawInterval;
-
-	    while (gt != null) {
-	        update();
-	        repaint();
-
-	        try {
-	            double remainingTime = nextDrawTime - System.nanoTime();
-	            remainingTime /= 1000000;
-
-	            if (remainingTime < 0) {
-	                remainingTime = 0;
-	            }
-
-	            Thread.sleep((long) remainingTime);
-
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-
-	        nextDrawTime += drawInterval;
-	    }
+		// TODO Auto-generated method stub
+		// game loop
+		double interval = 1000000000.0/FPS; // how long is a frame
+		double dt = 0; // delta time keeps track of how many time has passed
+		long prevTime = System.nanoTime(); // previous loop time
+//		long currentTime; 
+		
+		while (gt != null) {
+			long currentTime = System.nanoTime(); 
+			
+			dt += (currentTime - prevTime) / interval; // how much time has passed
+			prevTime = currentTime; // remember time for next loop
+			
+			// enough time has passed and repeat
+			if (dt >= 1) {
+				update();
+				repaint();
+				dt--;
+			}
+		}
 	}
+	
+	// sleep loop for lower CPU usage
+//	@Override
+//	public void run() {
+//	    double drawInterval = 1000000000.0 / FPS;
+//	    double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//	    while (gt != null) {
+//	        update();
+//	        repaint();
+//
+//	        try {
+//	            double remainingTime = nextDrawTime - System.nanoTime();
+//	            remainingTime /= 1000000;
+//
+//	            if (remainingTime < 0) {
+//	                remainingTime = 0;
+//	            }
+//
+//	            Thread.sleep((long) remainingTime);
+//
+//	        } catch (InterruptedException e) {
+//	            e.printStackTrace();
+//	        }
+//
+//	        nextDrawTime += drawInterval;
+//	    }
+//	}
 	
 	public void update() {
 		
