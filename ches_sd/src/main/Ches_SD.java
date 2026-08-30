c71056de6963b85944c0926036aab2b050782be5
clrkxd <clarkiemelitar@gmail.com>
2026-08-30 09:43:11 +0800

    added app icon temporarily

--- ches_sd/src/main/Ches_SD.java
+++ ches_sd/src/main/Ches_SD.java
@@ -1,11 +1,18 @@
 package main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Ches_SD {
 
 	public static void main(String[] args) {
 		JFrame w = new JFrame("ches-sd");
+		
+		ImageIcon icon = new ImageIcon(Ches_SD.class.getResource("/piece/pawn.png"));
+
+		
+        w.setIconImage(icon.getImage());
+		
 		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		w.setResizable(false);
 		
